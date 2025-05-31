261a9bf05b2eceff37b0a78f3baad11f081ccb1b
zaid123za4 <zaidzaidm52@gmail.com>
2025-05-31 22:28:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -204,3 +204,16 @@ client.once('ready', () => {
 });
 
 client.login(TOKEN);
+
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Nova bot is running!');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Web server running on port ${PORT}`);
+});
