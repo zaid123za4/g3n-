f453da2465228b88f5bce8d2a14aa6b8c8c71f40
zaid123za4 <zaidzaidm52@gmail.com>
2025-06-24 21:41:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-javascript
 const { Client, GatewayIntentBits, Partials, AttachmentBuilder, EmbedBuilder, ChannelType } = require('discord.js');
 const fs = require('fs');
 const path = require('path');
