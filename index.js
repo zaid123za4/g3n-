1687fe4babcfba3339e0ac94037588590d947f32
zaid123za4 <zaidzaidm52@gmail.com>
2025-06-25 18:15:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -228,7 +228,7 @@ async function handleDynamicGenCommand(msg, category) {
                     .setColor(0x2ecc71)
                     .setTitle('✨ Your Generated Stock Code!')
                     .setDescription(
-  `**🛡 FIRST VERIFY URSELF** [CLICK HERE](https://ember-chat-register-web.onrender.com/Chat)\n\n` +
+  `**🛡 FIRST VERIFY URSELF** [CLICK HERE](https://link-center.net/1359562/Tjl517CXY1Bv)\n\n` +
   `🎁 Here is your unique code for a **${category}** item: \`${hexCode}\`\n\n` +
   `🎫 To view details, open a ticket and ping our staff.`
 )                   
